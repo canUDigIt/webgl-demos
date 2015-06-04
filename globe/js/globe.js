a6ac216962c56d85a6580e586a775db15d60eb08
Tracy Brown <tracybrownjr@gmail.com>
2015-06-03 20:59:37 -0400

    Code cleanup and fixed the light controls

--- globe/js/globe.js
+++ globe/js/globe.js
@@ -8,8 +8,10 @@
         target = [0, 0, 0],
         lightPos = [2, 2, 0],
         programInfo = {},
+        viewMatrix = m4.inverse(m4.lookAt(eye, target, [0, 1, 0])),
+        modelMatrix = m4.identity(),
         uniforms = {
-            u_modelViewPerspectiveMatrix: m4.inverse(m4.lookAt(eye, target, [0, 1, 0])),
+            u_modelViewPerspectiveMatrix: m4.multiply(viewMatrix, modelMatrix),
             u_cameraLightPosition: [2, 2, 0],
             u_cameraEyePosition: eye,
             u_diffuseSpecularAmbientShininess: [0.9, 0.9, 0.1, 100],
@@ -20,16 +22,17 @@
         sphereBufferInfo = {},
         rotateStart = [0, 0, 0],
         rotateEnd = [0, 0, 0],
-        rotationSpeed = 0.01,
-        theta = Math.atan2(lightPos[0], lightPos[2]),
-        phi = Math.atan2(Math.sqrt(lightPos[0] * lightPos[0] + lightPos[2] * lightPos[2]), lightPos[1]),
-        thetaDelta = 0,
-        phiDelta = 0
-        maxTheta = 2 * Math.PI,
-        minTheta = 0,
-        minPhi = -Math.PI / 2.0,
-        maxPhi = Math.PI / 2.0,
+        rotationSpeed = 0.2,
+        lightTheta = Math.atan2(lightPos[0], lightPos[2]),
+        lightPhi = Math.atan2(Math.sqrt(lightPos[0] * lightPos[0] + lightPos[2] * lightPos[2]), lightPos[1]),
+        lightThetaDelta = 0,
+        lightPhiDelta = 0
+        maxLightTheta = 2 * Math.PI,
+        minLightTheta = 0,
+        minLightPhi = -Math.PI / 2.0,
+        maxLightPhi = Math.PI / 2.0,
         updateLight = false;
+
 
     function runApp() {
         var canvasName = "canvas-id",
@@ -51,8 +54,9 @@
         function dragged(event) {
             rotateEnd = [event.pageX, event.pageY, 0];
             var rotateDelta = v3.subtract(rotateEnd, rotateStart);
-            thetaDelta = 2 * Math.PI * (rotateDelta[0] / canvas.width) * rotationSpeed;
-            phiDelta = Math.PI * (rotateDelta[1] / canvas.height) * rotationSpeed;
+            lightThetaDelta = 2 * Math.PI * (rotateDelta[0] / canvas.width) * rotationSpeed;
+            lightPhiDelta = Math.PI * (rotateDelta[1] / canvas.height) * rotationSpeed;
+            rotateStart = rotateEnd
         }
 
         function mouseUp(event) {
@@ -79,7 +83,6 @@
 
     function load() {
         sphereBufferInfo = twgl.primitives.createCubeBufferInfo(gl, 2.0);
-        //uniforms.u_texture = twgl.createTexture(gl, { src: "images/2_no_clouds_4k.jpg" });
         uniforms.earthcolor = twgl.createTexture(gl, { src: "images/earthcolor.jpg" });
         uniforms.oceanmask = twgl.createTexture(gl, { src: "images/oceanmask.jpg" });
     }
@@ -90,15 +93,15 @@
 
         if (updateLight) {
             var radius = 2;
-            theta += thetaDelta;
-            phi += phiDelta;
+            lightTheta += lightThetaDelta;
+            lightPhi += lightPhiDelta;
 
-            theta = Math.max( minTheta, Math.min( maxTheta, theta ) );
-            phi  = Math.max( minPhi, Math.min( maxPhi, phi ) );
+            lightTheta = Math.max(minLightTheta, Math.min(maxLightTheta, lightTheta));
+            lightPhi  = Math.max(minLightPhi, Math.min(maxLightPhi, lightPhi));
 
-            var x = radius * Math.sin(phi) * Math.sin(theta);
-            var y = radius * Math.cos(phi);
-            var z = radius * Math.sin(phi) * Math.cos(theta);
+            var x = radius * Math.sin(lightPhi) * Math.sin(lightTheta);
+            var y = radius * Math.cos(lightPhi);
+            var z = radius * Math.sin(lightPhi) * Math.cos(lightTheta);
             uniforms.u_cameraLightPosition = [x, y, z];
         }
 
