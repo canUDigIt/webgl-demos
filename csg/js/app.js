e847dafbdf22257b5e8a27e08259b4031e5e42ee
Tracy Brown <tracybrownjr@gmail.com>
2015-06-27 22:51:20 -0500

    Starting on my CSG project

---
+++ csg/js/app.js
@@ -0,0 +1,26 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, 1., 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( 400, 400 );
+renderer.setClearColor( 0x000000 );
+
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+var render = function () {
+    requestAnimationFrame( render );
+
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+
+    renderer.render(scene, camera);
+};
+
+render();
