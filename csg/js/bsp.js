78131eb65dce24ca1121932d74d16359e8510105
Tracy Brown <tracybrownjr@gmail.com>
2015-07-17 23:47:32 -0400

    Changed from using arrays to objects for vectors and fleshed out a lot of the implementation for finding out if a point is inside an object defined by the BSP tree

--- csg/js/bsp.js
+++ csg/js/bsp.js
@@ -2,33 +2,26 @@ module.exports = (function(){
 
     var Vector = {
         negate : function(u) {
-            return u.map(function(value) {
-                return -value;
-            });
+            return { x: -u.x, y: -u.y, z: -u.z };
         },
         add : function(u, v) {
-            return u.map(function(value, index) {
-                return value + v[index];
-            });
+            return { x: u.x + v.x, y: u.y + v.y, z: u.z + v.z };
         },
         sub : function(u, v) {
             return this.add(u, this.negate(v));
         },
         dotProduct : function(u, v) {
-            return u[0]*v[0] + u[1]*v[1] + u[2]*v[2];
+            return u.x*v.x + u.y*v.y + u.z*v.z;
         },  
         crossProduct : function(u, v) {
-            // (u2v3 − u3v2, −(u1v3 − u3v1), u1v2 − u2v1)
-            return [u[1]*v[2] - u[2]*v[1], -(u[0]*v[2] - u[2]*v[0]), u[0]*v[1] - u[1]*v[0]];
+            return { x: u.y*v.z - u.z*v.y, y: -(u.x*v.z - u.z*v.x), z: u.x*v.y - u.y*v.x };
         },
         magnitude : function(u) {
             return Math.sqrt(this.dotProduct(u, u));
         },
         normalize : function(u) {
             var mag = this.magnitude(u);
-            return u.map(function(value) {
-                return value / mag;
-            });
+            return {x: u.x/mag, y: u.y/mag, z: u.z/mag};
         }
     };
 
@@ -51,7 +44,7 @@ module.exports = (function(){
 
     Plane.prototype.distance = function(u) {
         return Vector.dotProduct(this.normal, u) - this.d;
-    }
+    };
 
     function classifyPointToPlane(point, plane, e) {
         var distance = plane.distance(point);
@@ -74,8 +67,154 @@ module.exports = (function(){
         this.normal = normal;
     }
 
-    function BSPNode(polygons) {
-        this.polygons = polygons;
+    function selectSplittingPlane(polygons)
+    {
+        if (polygons.length === 0) {
+            return null;
+        }
+        var first = polygons.shift();
+        return Plane.createPlane(first.a, first.b, first.c);
+    }
+    function BSPNode(plane, leaf, solid) {
+        this.plane = plane;
+        this.leaf = leaf;
+        this.solid = solid;
+        this.front = null;
+        this.back = null;
+    }
+
+    BSPNode.prototype.isLeaf = function() {
+        return this.leaf;
+    };
+
+    BSPNode.prototype.isSolid = function() {
+        return this.solid;
+    };
+
+    function isPointInside(point, node) {
+        if (node.isLeaf()) {
+            return node.isSolid();
+        }
+        else {
+            var classification = classifyPointToPlane(point, node.plane);
+            if (classification == 1) {
+                return isPointInside(point, node.front);
+            }
+        }
+
+        return isPointInside(point, node.back);
+    }
+
+    function createFromPolygons(polygons) {
+        var node = null;
+        if (polygons.length === 0) return node;
+
+        if (polygons.length === 1) {
+            node = new BSPNode(Plane.createPlane(polygons[0].a, polygons[0].b, polygons[0].c), false, false);
+            node.front = new BSPNode(null, true, false);
+            node.back = new BSPNode(null, true, true);
+            return node;
+        }
+
+        var splitPlane = selectSplittingPlane(polygons);
+        var frontList = [],
+            backList = [];
+
+        polygons.forEach(function(polygon) {
+            switch(classifyPolygon(polygon, splitPlane))
+            {
+                case "coplanar":
+                case "front":
+                    frontList.push(polygon);
+                    break;
+
+                case "back":
+                    backList.push(polygon);
+                    break;
+
+                case "straddle":
+                    var result = splitPolygon(polygon);
+                    frontList.push(result.front);
+                    backList.push(result.back);
+                    break;
+            }
+        });
+
+        node = new BSPNode(splitPlane, false, false);
+
+        if (frontList.length !== 0) {
+            node.front = createFromPolygons(frontList);
+        }
+        else {
+            node.front = new BSPNode(null, true, false);
+        }
+        
+        if(backList.length !== 0) {
+            node.back = createFromPolygons(backList);
+        }
+        else {
+            node.back = new BSPNode(null, true, true);
+        }
+
+        return node;
+    }
+
+    function classifyPolygon(polygon, plane) {
+        var numberInFront = 0,
+            numberBehind = 0;
+
+        switch( classifyPointToPlane(polygon.a, plane, 0.1) )
+        {
+            case 1:
+                numberInFront++;
+                break;
+
+            case -1:
+                numberBehind++;
+                break;
+
+            default:
+        }
+
+        switch( classifyPointToPlane(polygon.b, plane, 0.1) )
+        {
+            case 1:
+                numberInFront++;
+                break;
+
+            case -1:
+                numberBehind++;
+                break;
+                
+            default:
+        }
+
+        switch( classifyPointToPlane(polygon.c, plane, 0.1) )
+        {
+            case 1:
+                numberInFront++;
+                break;
+
+            case -1:
+                numberBehind++;
+                break;
+                
+            default:
+        }
+
+        if (numberInFront !== 0 && numberBehind !== 0) {
+            return "straddle";
+        }
+
+        if (numberInFront !== 0) {
+            return "front";
+        }
+
+        if (numberBehind !== 0) {
+            return "behind";
+        }
+
+        return "coplanar";
     }
 
     return {
@@ -95,70 +234,14 @@ module.exports = (function(){
             });
         },
 
-        createFromPolygons : function(polygons) {
-            return new BSPNode(polygons);
-        },
+        createFromPolygons : createFromPolygons,
 
         allPolygons : function(node) {
             return node.polygons;
         },
 
-        classifyPolygon : function(polygon, plane) {
-            var numberInFront = 0,
-                numberBehind = 0;
+        classifyPolygon : classifyPolygon,
 
-            switch( classifyPointToPlane(polygon.a, plane, 0.1) )
-            {
-                case 1:
-                    numberInFront++;
-                    break;
-
-                case -1:
-                    numberBehind++;
-                    break;
-
-                default:
-            };
-
-            switch( classifyPointToPlane(polygon.b, plane, 0.1) )
-            {
-                case 1:
-                    numberInFront++;
-                    break;
-
-                case -1:
-                    numberBehind++;
-                    break;
-                    
-                default:
-            };
-
-            switch( classifyPointToPlane(polygon.c, plane, 0.1) )
-            {
-                case 1:
-                    numberInFront++;
-                    break;
-
-                case -1:
-                    numberBehind++;
-                    break;
-                    
-                default:
-            };
-
-            if (numberInFront != 0 && numberBehind != 0) {
-                return "straddle";
-            }
-
-            if (numberInFront != 0) {
-                return "front";
-            }
-
-            if (numberBehind != 0) {
-                return "behind";
-            }
-
-            return "coplanar";
-        }
+        isPointInside : isPointInside
     };
 })();
--- csg/tests/bsp-tests.js
+++ csg/tests/bsp-tests.js
@@ -7,68 +7,68 @@ var bsp = require('../js/bsp.js'),
 describe('Vector', function() {
     describe('#negate', function() {
         it('should return the negative of each vector element', function() {
-            var a = [1, 1, 1];
-            expect(bsp.Vector.negate(a)).to.eql([-1, -1, -1]);
+            var a = { x:1, y:1, z:1 };
+            expect(bsp.Vector.negate(a)).to.eql({ x: -1, y: -1, z: -1 });
         });
     });
     describe('#add', function() {
         it('should return the component to compnent sum vector', function(){
-            var a = [1, 0, 0];
-            var b = [0, 1, 0];
-            expect(bsp.Vector.add(a, b)).to.eql([1, 1, 0]);
+            var a = { x:1, y:0, z:0 };
+            var b = { x:0, y:1, z:0 };
+            expect(bsp.Vector.add(a, b)).to.eql({ x: 1, y: 1, z: 0 });
         });
     });
     describe('#sub', function() {
         it('should return the component to compnent difference vector', function(){
-            var a = [2, 5, 0];
-            var b = [0, 1, 0];
-            expect(bsp.Vector.sub(a, b)).to.eql([2, 4, 0]);
+            var a = { x:2, y:5, z:0 };
+            var b = { x:0, y:1, z:0 };
+            expect(bsp.Vector.sub(a, b)).to.eql({ x: 2, y: 4, z: 0 });
         });
     });
     describe('#dotProduct', function() {
         it('should return the dot product of two vectors', function() {
-            var a = [1, 0, 1];
-            var b = [1, 2, 3];
+            var a = { x:1, y:0, z:1 };
+            var b = { x:1, y:2, z:3 };
             expect(bsp.Vector.dotProduct(a, b)).to.be.equal(4);
         });
 
         it('should return 0 if vectors are perpendicular', function() {
-            var a = [1, 0, 0];
-            var b = [0, 1, 0];
+            var a = { x:1, y:0, z:0 };
+            var b = { x:0, y:1, z:0 };
             expect(bsp.Vector.dotProduct(a, b)).to.be.equal(0);
         });
 
         it('should return less than 0 if vectors are obtuse', function() {
-            var a = [1, 0, 0];
-            var b = [-1, 1, 0];
+            var a = { x:1, y:0, z:0 };
+            var b = { x:-1, y:1, z:0 };
             expect(bsp.Vector.dotProduct(a, b)).to.be.lessThan(0);
         });
 
         it('should return greater than 0 if vectors are acute', function() {
-            var a = [1, 0, 0];
-            var b = [1, 1, 0];
+            var a = { x:1, y:0, z:0 };
+            var b = { x:1, y:1, z:0 };
             expect(bsp.Vector.dotProduct(a, b)).to.be.greaterThan(0);
         });
     });
 
     describe('#crossProduct', function() {
         it('should return the correct direction and magnitude', function() {
-            var a = [1, 0, 0];
-            var b = [1, 1, 0];
+            var a = { x:1, y:0, z:0 };
+            var b = { x:1, y:1, z:0 };
 
             var sinTheta = Math.sin(Math.PI/4);
             var v = bsp.Vector.crossProduct(a, b);
 
             // For whatever reason chai thinks that -0 doesn't equal 0 only in
             // an array. If I use expect(0).to.equal(-0) that test passes...
-            expect(v).to.eql([0, -0, 1]);
+            expect(v).to.eql({ x:0, y:-0, z:1 });
             expect(bsp.Vector.magnitude(v)).to.equal(bsp.Vector.magnitude(a) * bsp.Vector.magnitude(b) * sinTheta);
         });
     });
 
     describe('#normalize', function() {
         it('should return a vector of length of 1', function() {
-            var a = [3, 4, 5];
+            var a = { x:3, y:4, z:5 };
 
             expect(Math.abs(bsp.Vector.magnitude(bsp.Vector.normalize(a)))).to.closeTo(1, 0.0001);
         });
@@ -82,9 +82,9 @@ describe('Plane', function() {
         c;
 
     before(function() {
-        a = [0, 0, 3];
-        b = [1, 0, 3];
-        c = [0, 1, 3];
+        a = { x:0, y:0, z:3 };
+        b = { x:1, y:0, z:3 };
+        c = { x:0, y:1, z:3 };
 
         plane = bsp.Plane.createPlane(a, b, c);
     });
@@ -92,7 +92,7 @@ describe('Plane', function() {
     describe('#createPlane', function(){
         it('should return a plane computed from three noncolinear points', function() {
             expect(plane).to.be.instanceof(bsp.Plane);
-            expect(plane.normal).to.eql([0, -0, 1]);
+            expect(plane.normal).to.eql({ x:0, y:-0, z:1 });
             expect(plane.d).to.equal(3);
         });
     });
@@ -105,11 +105,11 @@ describe('Plane', function() {
         });
 
         it('should return a positive value when points are in front of the plane', function() {
-            expect(plane.distance([1, 2, 5])).to.be.greaterThan(0);
+            expect(plane.distance({ x:1, y:2, z:5 })).to.be.greaterThan(0);
         });
 
         it('should return a negative value when points are behind the plane', function() {
-            expect(plane.distance([1, 2, -3])).to.be.lessThan(0);
+            expect(plane.distance({ x:1, y:2, z:-3 })).to.be.lessThan(0);
         });
     });
 });
@@ -138,36 +138,42 @@ describe('SolidBSP', function() {
         });
     });
 
-    describe('#createFromPolygons()', function(){
-        it('should return a BSP tree that contains all the polygons', function() {
+    describe('#isPointInside()', function() {
+        it('should return true if a point is inside solid space', function() {
             var rootNode = bsp.createFromPolygons(polygons);
-            expect(bsp.allPolygons(rootNode)).to.equal(polygons);
+            var point = { x:0, y:0, z:0 };
+            expect(bsp.isPointInside(point, rootNode)).to.be.true;
         });
 
+        it('should return false when a point is outside of solid space', function() {
+            var rootNode = bsp.createFromPolygons(polygons);
+            var point = { x:2, y:2, z:2 };
+            expect(bsp.isPointInside(point, rootNode)).to.be.false;
+        });
     });
 
-    describe('#classifyPolygon()', function(){
+    describe('#classifyPolygon()', function() {
         it('should return "front" if in front of the plane', function(){
-            var polygon = new bsp.Polygon([-1, 1, 0], [1, 0, 0],  [3, 1, 0], [0, 0, 1]);
-            var plane = new bsp.Plane([0, 1, 0], 0);
+            var polygon = new bsp.Polygon({ x:-1, y:1, z:0 }, { x:1, y:0, z:0 },  { x:3, y:1, z:0 }, { x:0, y:0, z:1 });
+            var plane = new bsp.Plane({ x:0, y:1, z:0 }, 0);
             expect(bsp.classifyPolygon(polygon, plane)).to.equal("front");
         });
 
         it('should return "behind" if behind the plane', function(){
-            var polygon = new bsp.Polygon([-1, -1, 0], [1, -3, 0],  [3, -1, 0], [0, 0, 1]);
-            var plane = new bsp.Plane([0, 1, 0], 0);
+            var polygon = new bsp.Polygon({ x:-1, y:-1, z:0 }, { x:1, y:-3, z:0 },  { x:3, y:-1, z:0 }, { x:0, y:0, z:1 });
+            var plane = new bsp.Plane({ x:0, y:1, z:0 }, 0);
             expect(bsp.classifyPolygon(polygon, plane)).to.equal("behind");
         });
 
         it('should return "straddle" if straddling the plane', function(){
-            var polygon = new bsp.Polygon([-1, 1, 0], [1, -1, 0],  [3, 1, 0], [0, 0, 1]);
-            var plane = new bsp.Plane([0, 1, 0], 0);
+            var polygon = new bsp.Polygon({ x:-1, y:1, z:0 }, { x:1, y:-1, z:0 },  { x:3, y:1, z:0 }, { x:0, y:0, z:1 });
+            var plane = new bsp.Plane({ x:0, y:1, z:0 }, 0);
             expect(bsp.classifyPolygon(polygon, plane)).to.equal("straddle");
         });
 
         it('should return "coplanar" if coplanar to the plane', function() {
-            var polygon = new bsp.Polygon([-1, 0, 0], [1, 0, 1],  [3, 0, 0], [0, 0, -1]);
-            var plane = new bsp.Plane([0, 1, 0], 0);
+            var polygon = new bsp.Polygon({ x:-1, y:0, z:0 }, { x:1, y:0, z:1 },  { x:3, y:0, z:0 }, { x:0, y:0, z:-1 });
+            var plane = new bsp.Plane({ x:0, y:1, z:0 }, 0);
             expect(bsp.classifyPolygon(polygon, plane)).to.equal("coplanar");
         });
     });
